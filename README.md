1b33f5b40479cf40e0f5ffe7f7da6bd17f70b5bc
Katie Jones <katie.jones@reactive-robotics.com>
2020-06-14 07:19:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Elevation to STL
+
+Use free SRTM 1-arc second data to generate STL files of any geographic region.
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Copyright (2020) Katie Jones. All rights reserved.
+
+from distutils.core import setup
+
+setup(
+    name='elevation-to-stl',
+    version='alpha',
+    description='Convert elevation data to STL file',
+    author='Katie Jones',
+    packages=['elevation-to-stl'],
+)
